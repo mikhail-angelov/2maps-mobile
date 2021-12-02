e532656797f7af30f2309466f665a332dd16591f
Sergey Sudaev <sudaevs@gmail.com>
2021-12-02 18:27:22 +0300

    marker colour depends on rate

--- app/components/MarksLocation.tsx
+++ app/components/MarksLocation.tsx
@@ -6,12 +6,15 @@ import { featureCollection } from '@turf/helpers';
 import { selectMarks } from "../reducers/marks";
 import { markToFeature } from "../actions/marks-actions";
 
+const colorPalette = ['#01579B', '#263238', '#F57F17', '#B71C1C', '#4A148C', '#1B5E20']
+
 const MarkStyle: CircleLayerStyle = {
-    circleRadius: 12,
-    circleColor: 'blue',
-    circleOpacity: 0.6,
+    circleRadius: 8,
+    circleColor: ['step', ['number', ['get', 'rate'], 0], colorPalette[0], 1, colorPalette[1], 2, colorPalette[2], 3, colorPalette[3], 4, colorPalette[4], 5, colorPalette[5]],
+    circleOpacity: 0.7,
     circleStrokeColor: 'white',
-    circleStrokeWidth: 0.5,
+    circleStrokeWidth: 4,
+    circleStrokeOpacity: 0.7,
 }
 
 interface MarksLocationProps {
