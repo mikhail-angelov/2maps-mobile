ca4f1f7c9d120cc238c399f78468b04dd6ccce34
Sergey Sudaev <sudaevs@gmail.com>
2021-12-06 14:43:39 +0300

    add making kml file from track

--- app/actions/tracker-actions.ts
+++ app/actions/tracker-actions.ts
@@ -5,6 +5,10 @@ import { LocationObject, hasServicesEnabledAsync, requestForegroundPermissionsAs
 import { ThreeAxisMeasurement } from 'expo-sensors';
 import { Track } from "../store/types";
 import { Position } from 'geojson';
+import { selectTracks } from "../reducers/tracker";
+import RNFS from 'react-native-fs'
+import DocumentPicker from 'react-native-document-picker';
+import { compileKml } from "../utils/kml";
 
 export const getLocation = async () => {
   const isEnabled = await hasServicesEnabledAsync()
@@ -79,3 +83,29 @@ export const stopTrackingAction = (): AppThunk => {
     dispatch({ type: ActionTypeEnum.EndTracking })
   };
 }
+
+export const exportTrackAction = (trackId: string): AppThunk => {
+  return async (dispatch, getState) => {
+    const tracks = selectTracks(getState())
+    const exportedTrack = tracks.find(item => item.id === trackId)
+    let url = ''
+    try {
+      if(!exportedTrack) {
+        throw 'can not find track by id'
+      }
+      const compiledKml = compileKml(exportedTrack)
+      url = RNFS.DocumentDirectoryPath + `/${compiledKml.name}.kml`;
+      console.log('writing kml to:', url, '\n')
+      await RNFS.writeFile(decodeURI(url), compiledKml.data, 'utf8')
+      Alert.alert('Track is saved', `to ${url}`)
+    } catch (err: any) {
+      console.log('Error write to:', url, '\n', err)
+      Alert.alert('Oops', `do not manage to save it ${url}`)
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
+    }
+  };
+};
--- app/utils/kml.ts
+++ app/utils/kml.ts
@@ -1,19 +1,26 @@
-const sample = '<?xml version="1.0" encoding="UTF-8"?>\
-<kml xmlns="http://www.opengis.net/kml/2.2">\
-<Document>\
-<name>track.kml</name>\
-<description>mapnn.bconf.com</description>\
-<open>1</open>\
-<Placemark>\
-<name>Document Feature 1</name>\
-<LineString>\
-<tessellate>1</tessellate>\
-<coordinates>-122.371,37.816,0</coordinates>\
-</LineString>\
-</Placemark>\
-</Document>\
-</kml>'
+import { Track } from "../store/types"
+import { Position } from 'geojson';
+import dayjs from 'dayjs'
 
-export const parseKml = () => {
+const addAltitude = (coordinates: Position) => ([...coordinates, 0])
 
+export const compileKml = (track: Track): {name: string, data: string} => {
+    const name = track.name || `track ${dayjs(track.start).format('YY.MM.DD HH-mm')}`
+    const coordinates = track.track.map(addAltitude).join(' ')
+    const data = `<?xml version="1.0" encoding="UTF-8"?>\
+    <kml xmlns="http://www.opengis.net/kml/2.2">\
+    <Document>\
+    <name>${name}.kml</name>\
+    <description>mapnn.bconf.com</description>\
+    <open>1</open>\
+    <Placemark>\
+    <name>${name}.kml</name>\
+    <LineString>\
+    <tessellate>1</tessellate>\
+    <coordinates>${coordinates}</coordinates>\
+    </LineString>\
+    </Placemark>\
+    </Document>\
+    </kml>`
+    return {name, data}
 }
