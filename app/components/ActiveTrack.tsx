305848ceb0a74f081c7e322bc57c553add173407
Sergei Sudaev <sudaevs@gmail.com>
2021-12-07 06:59:32 +0300

    show track on the map when track is selected

--- app/components/ActiveTrack.tsx
+++ app/components/ActiveTrack.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { State } from '../store/types'
 import MapboxGL, { LineLayerStyle } from "@react-native-mapbox-gl/maps";
 import { lineString } from '@turf/helpers';
 import { selectActiveTrack, selectSelectedTrack } from '../reducers/tracker'
-
+import { Position } from "geojson";
 
 const ActiveTrackStyle: LineLayerStyle = {
     lineCap: 'round',
@@ -24,16 +24,28 @@ const SelectedTrackStyle: LineLayerStyle = {
     lineOpacity: 0.84,
     lineColor: 'blue',
 }
+
+interface ActiveTrackProps{
+    onTrackSelect: (start: Position, end: Position) => void
+}
 const mapStateToProps = (state: State) => ({
     activeTrack: selectActiveTrack(state),
     selectedTrack: selectSelectedTrack(state),
 });
 const connector = connect(mapStateToProps)
-type Props = ConnectedProps<typeof connector>
+type Props = ConnectedProps<typeof connector> & ActiveTrackProps
 
-const ActiveTrack: FC<Props> = ({ activeTrack, selectedTrack }) => {
+const ActiveTrack: FC<Props> = ({ activeTrack, selectedTrack, onTrackSelect }) => {
     const activeRoute = activeTrack && activeTrack.track.length > 1 ? lineString(activeTrack.track) : null
     const selectedRoute = selectedTrack && selectedTrack.track.length > 1 ? lineString(selectedTrack.track) : null
+
+    useEffect(() => {
+        if (selectedRoute && selectedTrack) {
+            const start = selectedRoute.geometry.coordinates[0]
+            const end = selectedRoute.geometry.coordinates[selectedRoute.geometry.coordinates.length - 1]
+            onTrackSelect(start, end)
+        }
+    }, [selectedTrack])
 
     return (<>{activeRoute && <MapboxGL.ShapeSource id='active-track' shape={activeRoute}>
         <MapboxGL.LineLayer id='activeLineLayer' style={ActiveTrackStyle} minZoomLevel={1} />
