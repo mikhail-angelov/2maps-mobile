d8a106bf1b327a949733a46d0887d5c902069bb2
mikhail.angelov <mikhail.angelov@gmail.com>
2021-12-27 16:04:39 +0300

    fix config3

--- android/app/src/main/java/com/mapnnmobile/generated/BasePackageList.java
+++ android/app/src/main/java/com/mapnnmobile/generated/BasePackageList.java
@@ -12,7 +12,6 @@ public class BasePackageList {
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.location.LocationPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sensors.SensorsPackage()
     );
   }
