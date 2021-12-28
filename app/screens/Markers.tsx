82605bfc8750f35a80956f84aa87cc74df9171fe
Mikhail Tarasov <tarasovmikhail1103@gmail.com>
2021-12-28 16:56:38 +0300

    add filter for marks

--- app/screens/Markers.tsx
+++ app/screens/Markers.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useMemo } from "react";
-import { View, StyleSheet, Alert, Modal, TouchableOpacity } from "react-native";
+import React, { FC, useMemo, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Alert, Modal, TouchableOpacity } from "react-native";
 import { connect, ConnectedProps } from "react-redux";
 import { Button, ListItem } from 'react-native-elements';
 import { SwipeListView } from 'react-native-swipe-list-view';
@@ -13,7 +13,9 @@ import { selectMarks } from '../reducers/marks'
 import { importPoisAction, exportPoisAction, removeAllPoisAction, syncMarksAction, removeMarkCompletelyAction, editMarkAction } from '../actions/marks-actions'
 import { useTranslation } from "react-i18next";
 import Advertisement from "../components/AdMob";
+import MapModal from "../components/Modal";
 import { renderColor } from "../utils/formats";
+import { purple } from "../constants/color";
 
 interface OwnProps {
     center: Position;
@@ -45,6 +47,19 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 const Markers: FC<Props> = ({ markers, center, isAuthenticated, close, select, importPois, exportPois, removeAllPois, syncMarks, removeMark, editMark }) => {
     const { t } = useTranslation();
 
+    const list: Item[] = orderBy(markers, mark => distance(mark.geometry.coordinates, center, { units: 'kilometers' }))
+    .map(mark => ({
+        rate: mark.rate,
+        key: mark.id,
+        title: mark.name,
+        subtitle: `${distance(mark.geometry.coordinates, center, { units: 'kilometers' }).toFixed(2)} km, ${mark.description || ''}`,
+        mark,
+    }));
+
+    const [isFilterMarks, setIsFilterMarks] = useState<boolean>(false);
+    const [filterText, setFilterText] = useState<string>('');
+    const [filterList, setFilterList] = useState<Item[]>(list);
+
     const onRemoveAll = () => {
         Alert.alert(
             t('Warning!'),
@@ -66,15 +81,19 @@ const Markers: FC<Props> = ({ markers, center, isAuthenticated, close, select, i
             ]
         );
     }
-    const list: Item[] = orderBy(markers, mark => distance(mark.geometry.coordinates, center, { units: 'kilometers' }))
-        .map(mark => ({
-            rate: mark.rate,
-            key: mark.id,
-            title: mark.name,
-            subtitle: `${distance(mark.geometry.coordinates, center, { units: 'kilometers' }).toFixed(2)} km, ${mark.description || ''}`,
-            mark,
-        }))
     
+    const setFilterMarks = () => {
+        const newMarkList = filterText ? list.filter(item => item.title.includes(filterText) || item.subtitle.includes(filterText)) : list;
+        setFilterList(newMarkList);
+        setIsFilterMarks(false);
+    }
+
+    const setFilterReset = () => {
+        setFilterText('');
+        setFilterList(list);
+        setIsFilterMarks(false);
+    }
+
     const renderItem = ({ item }: { item: Item }) => (
         <TouchableOpacity
             activeOpacity={1}
@@ -111,6 +130,7 @@ const Markers: FC<Props> = ({ markers, center, isAuthenticated, close, select, i
     return <Modal style={styles.container} visible onRequestClose={close}>
         <View style={styles.wrapper}>
             <View style={styles.buttons}>
+                <Icon.Button style={styles.titleButton} backgroundColor="#fff0" name="search" onPress={() => setIsFilterMarks(true)} />
                 <Icon.Button style={styles.titleButton} backgroundColor="#fff0" name="file-download" onPress={exportPois} />
                 <Icon.Button style={styles.titleButton} backgroundColor="#fff0" name="file-upload" onPress={importPois} />
                 {isAuthenticated && <Icon.Button style={styles.titleButton} backgroundColor="#fff0" name="import-export" onPress={syncMarks} />}
@@ -119,18 +139,34 @@ const Markers: FC<Props> = ({ markers, center, isAuthenticated, close, select, i
             </View>
             <View style={styles.scroll}>
                 <SwipeListView
-                    data={list}
+                    data={filterList}
                     renderItem={memoizedValue}
                     renderHiddenItem={memoizedHiddenValue}
                     leftOpenValue={0}
                     rightOpenValue={-150}
-                    previewRowKey={list[0] ? list[0].mark.id : undefined}
+                    previewRowKey={filterList[0] ? filterList[0].mark.id : undefined}
                     previewOpenValue={-40}
                     previewOpenDelay={1000}
                 />
             </View>
         </View>
         <Advertisement />
+        {isFilterMarks && (
+            <MapModal onRequestClose={() => setIsFilterMarks(false)}>
+                <View style={styles.filterContainer}>
+                    <Text style={styles.inputLabel}>{t('Filter')}:</Text>
+                    <TextInput
+                        style={styles.modalInput}
+                        onChangeText={(value) => setFilterText(value)}
+                        value={filterText}
+                    />
+                    <View style={styles.buttonsRow}>
+                        <Button buttonStyle={styles.btn} title={t('Apply')} onPress={setFilterMarks} />
+                        <Button titleStyle={styles.inlineBtn} type='clear' onPress={setFilterReset} title={t('Clear')} />
+                    </View>
+                </View>
+            </MapModal>
+        )}
     </Modal>
 }
 
@@ -161,8 +197,8 @@ const styles = StyleSheet.create({
     buttons: {
         flexDirection: 'row',
         textAlign: 'center',
-        justifyContent: "flex-end",
-        padding: 10,
+        justifyContent: "space-between",
+        padding: 5,
         backgroundColor: '#303846',
     },
     titleButton: {
@@ -171,4 +207,35 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
     },
+    filterContainer: {
+        marginTop: 20,
+        width: '100%',
+    },
+    inputLabel: {
+        marginBottom: 5,
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    modalInput: {
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'grey',
+        marginBottom: 10,
+        width: '100%',
+    },
+    buttonsRow: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    btn: {
+        paddingHorizontal: 20,
+        backgroundColor: purple,
+    },
+    inlineBtn: {
+        color: purple,
+    }
 });
