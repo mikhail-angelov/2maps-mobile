fb1c8ecca4cdf42c1b8bd4ccced514ad599660f9
Sergey Sudaev <sudaevs@gmail.com>
2022-01-14 15:03:12 +0300

    add empty list text

--- app/constants/color.ts
+++ app/constants/color.ts
@@ -2,3 +2,4 @@ export const purple = '#6666FF';
 export const red = '#CC6666';
 export const green = '#669966';
 export const colorPalette = ['#263238', '#B71C1C', '#F57F17', '#1B5E20', '#01579B', '#4A148C'];
+export const emptyListText = '#444444'
