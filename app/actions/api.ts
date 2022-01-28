899ff6ef632cdf9734b993adfa5e29eb7106c399
mikhail.angelov <mikhail.angelov@gmail.com>
2022-01-28 18:10:39 +0300

    bump ver

--- app/actions/api.ts
+++ app/actions/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import FormData from "form-data";
-import { Buffer } from "buffer";
 
 export const HOST = 'https://2map.xyz'
 export const HOST_LOCAL = 'http://localhost:5555'
@@ -40,36 +39,15 @@ export const postLarge = async ({ url, token, data }: {
   const formData = new FormData()
   const value = JSON.stringify(data) //new Blob([JSON.stringify(data)]);
   formData.append('value', value,)
-
-  const headers = {
-    // 'Content-Length': value.length
-  }
-  try {
-    //   const r = await fetch(url, {
-
-    //     method:'POST',
-
-    //     // accept: 'application/json',
-
-    //     // 'Content-Type': 'multipart/form-data',
-
-    //     body: formData
-
-    // })
-    // console.log('---',r)
-    return await axios({
-      url,
-      method: 'post',
-      data: formData,
-      headers: Object.assign(headers, token ? {
-        authorization: `bearer ${token}`,
-        'Content-Type': 'multipart/form-data',
-      } : {}),
-    });
-  } catch (e) {
-    console.log('--', JSON.stringify(e))
-    throw e
-  }
+  return await axios({
+    url,
+    method: 'post',
+    data: formData,
+    headers: token ? {
+      authorization: `bearer ${token}`,
+      'Content-Type': 'multipart/form-data',
+    } : {},
+  });
 }
 
 export const getLocal = (url: string) => {
